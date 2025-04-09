15a7be259d79631095be9412d1d3fac0fa700dee
Apoorv P <apurvpara@gmail.com>
2025-04-09 17:05:03 +0530

    Create MergeSort.js
    Merge sort schema

---
+++ MergeSort.js
@@ -0,0 +1,15 @@
+const sentence = ["M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"];
+
+function merge(arrayToSort, auxillary, lo, mid, hi) {
+  
+}
+
+/*
+ * Divide and conquer approach for sorting
+ * auxillary array is an extra set of memory maintained
+ * for the sort to have Stability (retain order when sorting by multiple keys)
+*/
+
+function sort() {
+  
+}
