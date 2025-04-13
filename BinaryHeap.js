592fc57fb554178ac0b55fdb8d4cb3d95ffaca7d
Apoorv P <apurvpara@gmail.com>
2025-04-13 22:45:23 +0530

    Update BinaryHeap.js with alternate

--- BinaryHeap.js
+++ BinaryHeap.js
@@ -37,3 +37,41 @@ for (let j = Math.floor(N / 2); j >= 1; j--) {
 }
 
 console.log(numbers)
+
+
+
+/* ALTERNATE
+// How many lesser numbers on the right of a number in the list?
+
+const numbers = [null, 4, 8, 7, 1, 2, 3];
+let k = numbers.length - 1;
+let N = numbers.length - 1;
+
+function swap (arr, index1, index2) {
+  const current = arr[index1];
+
+  arr[index1] = arr[index2];
+  arr[index2] = current;
+  //console.log(arr, index1, index2)
+}
+
+for (let j = Math.floor(N / 2); j >= 1; j--) {
+  while (2*j <= N) {
+    let l = 2*j;
+    console.log('j = ', j, 'l = ', l, 'Numbers = ', numbers[j], numbers[l] , numbers[l + 1])
+    if (l < N && numbers[l] < numbers[l + 1]) {
+      l++;
+    }
+
+    if (numbers[j] > numbers[l]) {
+      break;
+    }
+
+    swap(numbers, j, l);
+    j = l;
+  }
+  
+}
+
+console.log(numbers)
+*/
