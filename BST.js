79a3666eb56e24411000d53946060936caf4f82f
Apoorv P <apurvpara@gmail.com>
2025-04-21 18:11:38 +0530

    Create BST.js

---
+++ BST.js
@@ -0,0 +1,49 @@
+class Node {
+  key;
+  value;
+  left;
+  right;
+  
+  constructor(k, v) {
+    this.key = k;
+    this.value = v;
+  }
+}
+
+class BST {
+  #root  
+  constructor() {
+    
+  }
+
+  insertNode(node, k, v) {
+    if (node === null) {
+      return new Node(k, v);
+    }
+
+    if (key < node.key) {
+      node.left = insertNode(node.left, k, v);
+    } else if (key > node.key) {
+      node.right = insertNode(node.right, k, v);
+    } else {
+      node.value = v;
+    }
+
+    return node;
+  }
+
+  getNodeVal(key) {
+    let tracer = this.root;
+    while (tracer != null) {
+      if (key < tracer.key) {
+        tracer = tracer.left;
+      } else if (key > tracer.key) {
+        tracer = tracer.right
+      } else {
+        return tracer.value;
+      }
+    }
+
+    return null;
+  }
+}
